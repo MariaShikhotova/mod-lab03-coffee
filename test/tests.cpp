b01c02698704d5c86a57bc564ac1a15829e15570
MariaShikhotova <126818543+MariaShikhotova@users.noreply.github.com>
2023-03-23 21:14:32 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,3 +2,54 @@
 
 #include <gtest/gtest.h>
 #include "Automata.h"
+TEST(Automata, TestOn) {
+  Automata automata();
+  automata.on();
+  EXPECT_EQ(automata.getState(), "WAIT");
+}
+TEST(Automata, TestOff) {
+  Automata automata();
+  automata.on();
+  automata.off();
+  EXPECT_EQ(automata.getState(), "OFF");
+}
+TEST(Automata, TestWait) {
+  Automata automata();
+  automata.on();
+  automata.coin(200);
+  automata.cancel();
+  EXPECT_EQ(automata.getState(), "WAIT");
+}
+TEST(Automata, TestFinish) {
+  Automata automata();
+  automata.on();
+  automata.coin(200);
+  automata.choice(2);
+  EXPECT_EQ(automata.getState(), "WAIT");
+}
+
+TEST(Automata, TestMenu) {
+  Automata automata();
+  automata.on();
+  automata.coin(200);
+  automata.choice(10);
+  EXPECT_EQ(automata.getState(), "WAIT");
+}
+TEST(Automata, TestCash) {
+  Automata automata();
+  automata.on();
+  automata.coin(10);
+  automata.choice(2);
+  EXPECT_EQ(automata.getState(), "WAIT");
+}
+TEST(Automata, TestCoin) {
+  Automata automata();
+  automata.on();
+  automata.choice(2);
+  EXPECT_EQ(automata.getState(), "WAIT");
+}
+TEST(Automata, TestAccept) {
+  Automata automata();
+  automata.coin(150);
+  EXPECT_EQ(automata.getState(), "ACCEPT");
+}
