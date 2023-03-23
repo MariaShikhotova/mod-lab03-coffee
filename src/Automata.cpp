f218f678fcaa26792852cd64926643fc1c8e493b
MariaShikhotova <126818543+MariaShikhotova@users.noreply.github.com>
2023-03-23 22:41:09 +0300

    Update Automata.cpp

--- src/Automata.cpp
+++ src/Automata.cpp
@@ -2,7 +2,7 @@
 #include "Automata.h"
 Automata::Automata() {
   count_menu = 3;
-  menu = new string[count_menu]{"Чай", "Кофе с молоком", "Какао"};
+  menu = new std::string[count_menu]{"Чай", "Кофе с молоком", "Какао"};
   prices = new int[count_menu]{30, 50, 45};
   state = OFF;
   cash = 0;
@@ -21,14 +21,14 @@ void Automata::coin(int sum) {
 }
 void Automata::getMenu() {
   for (int i = 0; i < count_menu; i++) {
-    cout << i + 1 << "  " << menu[i] << " " << prices[i] << "\n";
+    std::cout << i + 1 << "  " << menu[i] << " " << prices[i] << "\n";
   }
 }
 Automata::State Automata::getState() { return state; }
 void Automata::choice(int num) {
   if (state == ACCEPT) {
     if (num < 1 || num > count_menu) {
-      cout << "Неправильно выбран напиток\n";
+      std::cout << "Неправильно выбран напиток\n";
     } else {
       state = CHECK;
       check(num);
@@ -38,7 +38,7 @@ void Automata::choice(int num) {
 void Automata::check(int num) {
   state = CHECK;
   if (cash < prices[num - 1]) {
-    cout << "Недостаточно денег\n";
+    std::cout << "Недостаточно денег\n";
     state = WAIT;
   } else {
     cook(num);
@@ -49,19 +49,18 @@ void Automata::cancel() {
 }
 void Automata::cook(int num) {
   state = COOK;
-  cout << "Приготовление напитка...\n";
   switch (num) {
     case 1:
-      Sleep(5000);
+      std::cout << "Приготовление напитка 1\n";
     case 2:
-      Sleep(8000);
+      std::cout << "Приготовление напитка 2\n";
     case 3:
-      Sleep(10000);
+      std::cout << "Приготовление напитка 3\n";
   }
   finish(cash - prices[num - 1]);
 }
 void Automata::finish(int s) {
-  cout << "Напиток готов\n";
+  std::cout << "Напиток готов\n";
   if (s > 0) cout << "Сдача: " << s << "\n";
   state = WAIT;
 }
