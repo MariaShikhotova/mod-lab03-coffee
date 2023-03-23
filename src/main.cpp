d1cf57d6ad2c7edd7185804a5848836c8cffd9fa
MariaShikhotova <126818543+MariaShikhotova@users.noreply.github.com>
2023-03-23 21:17:16 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include "windows.h"
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    Automata p;
+    p.getMenu();
+    p.on();
+    p.coin(200);
+    p.choice(2);
+    
+}
