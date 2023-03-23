812ccca1f7802b8ed1de0b8f20512113078bbdb8
MariaShikhotova <126818543+MariaShikhotova@users.noreply.github.com>
2023-03-23 21:18:36 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <string>
 #include "windows.h"
+#include "Automata.h"
 using namespace std;
-int main()
-{
-    setlocale(LC_ALL, "Russian");
-    Automata p;
-    p.getMenu();
-    p.on();
-    p.coin(200);
-    p.choice(2);
-    
+int main() {
+  setlocale(LC_ALL, "Russian");
+  Automata p;
+  p.getMenu();
+  p.on();
+  p.coin(200);
+  p.choice(2);
 }
